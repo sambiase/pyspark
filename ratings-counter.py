e4fd07440c008064336e51b504ae134c07776b1b
Martinho Sambiase <martinho.sambiase@dremio.com>
2021-12-09 19:56:10 -0300

    "added more py files"

--- ratings-counter.py
+++ ratings-counter.py
@@ -4,7 +4,10 @@ import collections
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
+# lines is an RDD object that contains the dataset u.data
 lines = sc.textFile("file:///home/sambiase/courses/SparkCourse/ml-100k/u.data")
+
+# ratings is a new RDD with all ratings values
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
--- total_spent_by_customer.py
+++ total_spent_by_customer.py
@@ -1,3 +1,5 @@
+# find total amount spent by customer ID
+
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("Total amount spent by customer")
@@ -8,8 +10,14 @@ def splitLine(line):
     fields = line.split(',')     # splits the lines by , from 1800.csv
     customerID = int(fields[0])      # gets id from field pos 0
     amountSpent = float(fields[2])        # gets entryType TMIN or TMAX from field pos 2
-    return (customerID,amountSpent)
+    return (customerID, amountSpent)
 
 
-lines = sc.textFile("file:///home/sambiase/courses/SparkCourse/customer-orders.csv")
-splitLines = lines.map(splitLine)
+input = sc.textFile("file:///home/sambiase/courses/SparkCourse/customer-orders.csv")
+splitLines = input.map(splitLine)
+totalByCustomer = splitLines.reduceByKey(lambda x,y:x+y)
+
+results = totalByCustomer.collect()
+
+for i in results:
+    print (i)
